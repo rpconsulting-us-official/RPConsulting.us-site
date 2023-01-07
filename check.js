d1c71d002256ca80944c045fc0402e22a4eadf28
Riley Pinney <rileypinney+git@rpconsulting.us>
2023-01-06 22:19:14 -0800

    Delete check.js

--- check.js
+++
@@ -1,9 +0,0 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-
-    if (username === "u1ser" && password === "web_dev") {
-
-    } else {
-	window.location.assigh("/index.html");
-        loginErrorMsg.style.opacity = 1;
-    }
