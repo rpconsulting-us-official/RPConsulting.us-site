9d280bb1b21b4051de7885ab94fe180750c4d6a0
Riley Pinney <rileypinney+git@rpconsulting.us>
2023-01-06 22:19:13 -0800

    Delete checkusr.js

--- checkusr.js
+++
@@ -1,9 +0,0 @@
-function checkCookie() {
-    let user = document.cookie;
-    if (user = "yes") {
-    alert("Welcome again admin");
-    } else {
-    alert("goodbye non-admin");
-    } 
-    }
-checkCookie()
